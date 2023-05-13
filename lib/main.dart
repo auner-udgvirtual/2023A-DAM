0fe156e56e6899ccafb425c2f7bcfca998b6d0a2
amedina1-godaddy <amedina1@godaddy.com>
2023-05-12 19:19:44 -0600

    3.6. Peticiones HTTP

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,21 @@
 import 'package:flutter/material.dart';
+import './models/post/post_list.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Catalogo de Peliculas',
+      title: 'HTTP',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: WelcomeScreen(),
-    );
-  }
-}
-
-class WelcomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          // Configuramos la imagen de fondo
-          image: DecorationImage(
-            image: AssetImage('../assets/movies_wallpaper.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Muestra el mensaje de bienvenida
-              Text(
-                '¡Bienvenido!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              // Muestra el nombre de la aplicación
-              Text(
-                'Catalogo de Peliculas',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: PostsPage(),
     );
   }
 }
