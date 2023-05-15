97e1e5228eaee92041e85badd5a43570b9c2c7b8
amedina1-godaddy <amedina1@godaddy.com>
2023-05-14 18:59:56 -0600

    3.7. Integración de base de datos

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,71 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
-import './models/post/post_list.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'HTTP',
+    return const MaterialApp(
+      // Remove the debug banner
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: PostsPage(),
+      title: '3.7 DB Integration',
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(controller: controller),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              final name = controller.text;
+
+              createUser(name: name);
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Future createUser({required String name}) async {
+    final docUser =
+        FirebaseFirestore.instance.collection('catalogo').doc('my-id');
+
+    final json = {
+      'name': name,
+      'age': 21,
+      'birthday': DateTime(2001, 7, 28),
+    };
+
+    await docUser.set(json);
   }
 }
